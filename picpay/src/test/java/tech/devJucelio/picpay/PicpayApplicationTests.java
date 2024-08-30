1a75e9a8757a3c9a0c32bb8fb4a97b4eb58df4f5
JUCELIO <juceliocoelho2022@gmail.com>
2024-08-30 06:28:34 -0300

    Initial commit

---
+++ picpay/src/test/java/tech/devJucelio/picpay/PicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devJucelio.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
